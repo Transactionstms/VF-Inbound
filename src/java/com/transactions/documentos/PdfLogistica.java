74953a2b6b29299897604c10938a4a1e41f58de6
grecendiz24 <grecendiz24@gmail.com>
2023-06-01 11:19:18 -0600

    creacion pdf logistica

---
+++ src/java/com/transactions/documentos/PdfLogistica.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.transactions.documentos;
+
+/**
+ *
+ * @author grecendiz
+ */
+public class PdfLogistica {
+
+    public PdfLogistica() {
+    }
+    
+    
+     public boolean CrearPdfLogistica(String agrupador) {
+         boolean pdf=true;
+         
+         
+        return pdf; 
+    }
+    
+    
+    
+    
+    
+}
