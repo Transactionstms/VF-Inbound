cf384b5515c645dcc591a17fead096e9e3671206
Mateos Cruz <42045991+MATEOSCRUZ@users.noreply.github.com>
2024-09-25 11:15:31 -0600

    Se actualiza nomeclaturas en selección de fechas - customs

--- web/lib/validationsInbound/customs/validacionesCeldas.js
+++ web/lib/validationsInbound/customs/validacionesCeldas.js
@@ -358,7 +358,7 @@ function formatoFecha(valor) {
   // Convertir el mes numérico a texto
   var mesTexto = '';
   var mesNumerico = parseInt(fechaFiltrada.slice(0, 2), 10);
-  var meses = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
+  var meses = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   if (mesNumerico >= 1 && mesNumerico <= 12) {
     mesTexto = meses[mesNumerico - 1];
   }
